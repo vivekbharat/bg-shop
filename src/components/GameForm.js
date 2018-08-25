40dc45b7005b88ee7d58011d93c000cd08196e44
Vivek Ravikumar <vr329@njit.edu>
2018-08-25 09:07:59 -0700

    textare field and universal event handler

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 
 class GameForm extends Component {
   state = {
-    name: ""
+    name: "",
+    description: ""
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log({ title: this.state.name });
+    console.log({ title: this.state });
   };
 
   onChange = e => {
-    this.setState({ name: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
@@ -23,12 +24,27 @@ class GameForm extends Component {
             <input
               type="text"
               id="name"
+              name="name"
               placeholder="Full Game Title"
               // ref={input => (this.name = input)}
               value={this.state.name}
               onChange={this.onChange}
             />
           </div>
+
+          <div className="field">
+            <label htmlFor="description">Game Description</label>
+            <textarea
+              type="text"
+              id="description"
+              name="description"
+              placeholder="Full Game Description"
+              // ref={input => (this.description = input)}
+              value={this.state.description}
+              onChange={this.onChange}
+            />
+          </div>
+
           <button className="ui button" type="submit">
             Create
           </button>
